10ec9bbeb367baee48951d9ec7a9e475b46f2217
FMadruga <madruga.francisco@protonmail.com>
2022-06-16 10:31:35 +0100

    data prep

--- src/main.py
+++ src/main.py
@@ -5,44 +5,111 @@ from pytorch_lightning import Trainer
 from torchvision import transforms
 from resnet import ResNet
 from pytorch_lightning.callbacks.early_stopping import EarlyStopping
-from utils import load_image_datasets, get_dataloaders, plotting_time_normalization
+from utils import load_image_datasets, get_dataloaders, plotting_time_normalization, black_and_white_image_to_binary, split_train_dataset_to_val
+from constants import TRAIN_NEW_MODELS, DATASETS_WITH_BAD_IMAGE_RESOLUTIONS, DATASET_TYPES, DATASETS_PATH, GPUS, RESULTS, FAST_DEV_RUN
 
 
-RESULTS = 'results.csv'
-TRAIN_NEW_MODELS = False
-DATASETS_WITH_BAD_IMAGE_RESOLUTIONS = ['Computers', 'Earthquakes', 'ElectricDevices', 'FreezerRegularTrain', 'FreezerSmallTrain', 'Haptics', 'HouseTwenty', 'InlineSkate', 'InsectEPGRegularTrain', 'InsectEPGSmallTrain', 'LargeKitchenAppliances', 'Lightning2', 'Lightning7', 'Phoneme', 'RefrigerationDevices', 'ScreenType', 'SemgHandGenderCh2', 'SemgHandMovementCh2', 'SemgHandSubjectCh2', 'ShapeletSim']
+# def train_resnet50(datasets_parameters, data_type, results):
+#     if 'resnet50_train_accuracy' not in results.columns or TRAIN_NEW_MODELS:
+#         print('Never trained ResNet50')
+#         results['resnet50_train_accuracy'] = np.full((len(results)), -1).tolist()
+#         results['resnet50_val_accuracy'] = np.full((len(results)), -1).tolist()
+#         results['resnet50_test_accuracy'] = np.full((len(results)), -1).tolist()
+#         results.to_csv(RESULTS, index=False)
+#     model_parameters = pd.read_excel(open('datasets_and_model_parameters.xlsx', 'rb'), sheet_name='resnet_parameters')
+#     train_transform = transforms.Compose([lambda x: plotting_time_normalization(x), transforms.ToTensor()])
+#     val_transform = transforms.Compose([lambda x: plotting_time_normalization(x), transforms.ToTensor()])
+#     test_transform = transforms.Compose([lambda x: plotting_time_normalization(x), transforms.ToTensor()])
+#     for i, dataset in datasets_parameters.iterrows():
+#         num_epochs = int(model_parameters.iloc[i]['resnet50_epochs'])
+#         batch_size = int(model_parameters.iloc[i]['resnet50_batch_size'])
+#         learning_rate = float(model_parameters.iloc[i]['resnet50_learning_rate'])
+#         early_stop_patience = int(model_parameters.iloc[i]['resnet50_early_stop_patience'])
+#         early_stop_callback = EarlyStopping(monitor='val_loss', min_delta=.0, patience=early_stop_patience, verbose=True, mode='min')
+#         print(f'[{i + 1}/{len(datasets_parameters)}] ResNet50 for {dataset["name"]}: num_epochs = {num_epochs}, batch_size = {batch_size}, learning_rate = {learning_rate}, early_stop_patience = {early_stop_patience}')
+#         if dataset["name"] not in DATASETS_WITH_BAD_IMAGE_RESOLUTIONS:
+#             dataset_path = os.path.join(DATASETS_PATH, dataset['name'], data_type)
+#             has_validation_dataset = True
+#             if not os.path.isdir(os.path.join(dataset_path, 'val')):
+#                 has_validation_dataset = False
+#             if has_validation_dataset:
+#                 train_dataset, val_dataset, test_dataset = load_image_datasets(dataset_path, train_transform, test_transform, val_transform)
+#                 train_loader, val_loader, test_loader = get_dataloaders(batch_size, train_dataset, test_dataset, val_dataset)
+#             else:
+#                 train_dataset, test_dataset = load_image_datasets(dataset_path, train_transform, test_transform)
+#                 train_loader, test_loader = get_dataloaders(batch_size, train_dataset, test_dataset)
+#             model = None
+#             trainer = Trainer(callbacks=[early_stop_callback], gpus=GPUS, max_epochs=num_epochs, fast_dev_run=False)
+#             if results.iloc[i]['resnet50_train_accuracy'] == -1:
+#                 model = ResNet.ResNet50(img_channels=1, num_classes=dataset['num_classes'], learning_rate=learning_rate)
+#                 if has_validation_dataset:
+#                     trainer.fit(model, train_loader, val_loader)
+#                 else:
+#                     trainer.fit(model, train_dataloaders=train_loader)
+#                 results.loc[i,'resnet50_train_accuracy'] = trainer.test(model, train_loader)[0]['test_accuracy']
+#                 results.loc[i,'resnet50_val_accuracy'] = trainer.test(model, val_loader)[0]['test_accuracy']
+#                 results.loc[i,'resnet50_test_accuracy'] = trainer.test(model, test_loader)[0]['test_accuracy']
+#                 results.to_csv(RESULTS, index=False)
+#             else:
+#                 print('Model already trained')
+#         else:
+#             print('Bad image dimension')
+#     results.to_csv(RESULTS, index=False)
+#     return results
 
-def train_resnet50(datasets_parameters, results):
+def train_resnet50(datasets_parameters, data_type, results):
+    # transformers
+    train_transform = transforms.Compose([lambda x: black_and_white_image_to_binary(x), transforms.ToTensor()])
+    val_transform = transforms.Compose([lambda x: black_and_white_image_to_binary(x), transforms.ToTensor()])
+    test_transform = transforms.Compose([lambda x: black_and_white_image_to_binary(x), transforms.ToTensor()])
+    if data_type == 'line_plots':
+        train_transform = transforms.Compose([lambda x: plotting_time_normalization(x), transforms.ToTensor()])
+        val_transform = transforms.Compose([lambda x: plotting_time_normalization(x), transforms.ToTensor()])
+        test_transform = transforms.Compose([lambda x: plotting_time_normalization(x), transforms.ToTensor()])
+    # add column for model results
     if 'resnet50_train_accuracy' not in results.columns or TRAIN_NEW_MODELS:
-        print('Never trained ResNet50')
+        print('Never trained this model')
         results['resnet50_train_accuracy'] = np.full((len(results)), -1).tolist()
         results['resnet50_val_accuracy'] = np.full((len(results)), -1).tolist()
         results['resnet50_test_accuracy'] = np.full((len(results)), -1).tolist()
         results.to_csv(RESULTS, index=False)
     model_parameters = pd.read_excel(open('datasets_and_model_parameters.xlsx', 'rb'), sheet_name='resnet_parameters')
-    train_transform = transforms.Compose([lambda x: plotting_time_normalization(x), transforms.ToTensor()])
-    val_transform = transforms.Compose([lambda x: plotting_time_normalization(x), transforms.ToTensor()])
-    test_transform = transforms.Compose([lambda x: plotting_time_normalization(x), transforms.ToTensor()])
+    # training model
     for i, dataset in datasets_parameters.iterrows():
-        print(f'[{i + 1}/{len(datasets_parameters)}] training ResNet50 for dataset {dataset["name"]}')
+        # model parameters
         num_epochs = int(model_parameters.iloc[i]['resnet50_epochs'])
         batch_size = int(model_parameters.iloc[i]['resnet50_batch_size'])
         learning_rate = float(model_parameters.iloc[i]['resnet50_learning_rate'])
         early_stop_patience = int(model_parameters.iloc[i]['resnet50_early_stop_patience'])
         early_stop_callback = EarlyStopping(monitor='val_loss', min_delta=.0, patience=early_stop_patience, verbose=True, mode='min')
-        print(f'num_epochs = {num_epochs}, batch_size = {batch_size}, learning_rate = {learning_rate}, early_stop_patience = {early_stop_patience}')
+        print(f'[{i + 1}/{len(datasets_parameters)}] RESNET50 {dataset["name"]}: num_epochs = {num_epochs}, batch_size = {batch_size}, learning_rate = {learning_rate}, early_stop_patience = {early_stop_patience}')
         if dataset["name"] not in DATASETS_WITH_BAD_IMAGE_RESOLUTIONS:
-            dataset_path = '######################################################################'
-            train_dataset, val_dataset, test_dataset = load_image_datasets(dataset_path, train_transform, test_transform, val_transform)
-            train_loader, val_loader, test_loader = get_dataloaders(batch_size, train_dataset, test_dataset, val_dataset)
+            dataset_path = os.path.join(DATASETS_PATH, dataset['name'], data_type)
+            train_dataset = None
+            test_dataset = None
+            val_dataset = None
+            train_loader = None
+            test_loader = None
+            val_loader = None
+            has_val_dataset = os.path.isdir(os.path.join(dataset_path, 'val'))
+            if has_val_dataset:
+                train_dataset, val_dataset, test_dataset = load_image_datasets(dataset_path, train_transform, test_transform, val_transform)
+                train_loader, val_loader, test_loader = get_dataloaders(batch_size, train_dataset, test_dataset, val_dataset)
+            else:
+                train_dataset, test_dataset = load_image_datasets(dataset_path, train_transform, test_transform)
+                train_loader, test_loader = get_dataloaders(batch_size, train_dataset, test_dataset)
             model = None
-            trainer = Trainer(callbacks=[early_stop_callback], gpus=1, max_epochs=num_epochs, fast_dev_run=False)
+            trainer = Trainer(callbacks=[early_stop_callback], gpus=GPUS, max_epochs=num_epochs, fast_dev_run=FAST_DEV_RUN)
             if results.iloc[i]['resnet50_train_accuracy'] == -1:
                 model = ResNet.ResNet50(img_channels=1, num_classes=dataset['num_classes'], learning_rate=learning_rate)
-                trainer.fit(model, train_loader, val_loader)
+                if has_val_dataset:
+                    trainer.fit(model, train_loader, val_loader)
+                else:
+                    trainer.fit(model, train_loader)
                 results.loc[i,'resnet50_train_accuracy'] = trainer.test(model, train_loader)[0]['test_accuracy']
-                results.loc[i,'resnet50_val_accuracy'] = trainer.test(model, val_loader)[0]['test_accuracy']
                 results.loc[i,'resnet50_test_accuracy'] = trainer.test(model, test_loader)[0]['test_accuracy']
+                if has_val_dataset:
+                    results.loc[i,'resnet50_val_accuracy'] = trainer.test(model, val_loader)[0]['test_accuracy']
                 results.to_csv(RESULTS, index=False)
             else:
                 print('Model already trained')
@@ -56,7 +123,9 @@ def main():
     results = pd.DataFrame(datasets_parameters['name'])
     if os.path.exists(RESULTS):
         results = pd.read_csv(RESULTS)
-    train_resnet50(datasets_parameters, results)
+    for data_type in DATASET_TYPES:
+        split_train_dataset_to_val(datasets_parameters, data_type)
+        results = train_resnet50(datasets_parameters, data_type, results)
 
 if __name__ == '__main__':
     main()
--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,8 @@ import torch
 import numpy as np
 from PIL import Image
 from torchvision import datasets
+from sklearn.model_selection import train_test_split
+from constants import DATASETS_PATH
 
 
 def plotting_time_normalization(img, treshold=170):
@@ -12,6 +14,13 @@ def plotting_time_normalization(img, treshold=170):
     thresh, img_array = cv2.threshold(img_array, treshold, 255, cv2.THRESH_BINARY)
     img_array[img_array == 0] = [1]
     img_array[img_array == 255] = [0]
+    img = Image.fromarray(img_array)
+    return img
+
+def black_and_white_image_to_binary(img):
+    img_array = np.array(img)
+    img_array[img_array == 0] = [0]
+    img_array[img_array == 255] = [1]
     img = Image.fromarray(img_array)
     return img
 
@@ -30,3 +39,43 @@ def get_dataloaders(batch_size, train_dataset, test_dataset, val_dataset=None):
         return train_loader, test_loader
     val_loader = torch.utils.data.DataLoader(dataset=val_dataset, batch_size=batch_size, shuffle=False)
     return train_loader, val_loader, test_loader
+
+def split_train_dataset_to_val(datasets_parameters, data_type):
+    for i, dataset in datasets_parameters.iterrows():
+        val_path = os.path.join(DATASETS_PATH, dataset['name'], data_type, 'val')
+        train_path = os.path.join(DATASETS_PATH, dataset['name'], data_type, 'train')
+        if os.path.isdir(val_path):
+            for dataset_class in os.listdir(val_path):
+                val_class_path = os.path.join(val_path, dataset_class)
+                for img in os.listdir(val_class_path):
+                    img_path = os.path.join(val_class_path, img)
+                    path_splits = img_path.split('/')
+                    path_splits[3] = path_splits[3].replace('val', 'train')
+                    class_path = '/'.join(path_splits[:5])
+                    if not os.path.isdir(class_path):
+                        os.mkdir(class_path)
+                    new_path = '/'.join(path_splits)
+                    os.rename(img_path, new_path)
+                os.rmdir(val_class_path)
+        else:                    
+            os.mkdir(val_path)
+        imgs = []
+        for dataset_class in os.listdir(train_path):
+                val_class_path = os.path.join(val_path, dataset_class)
+                train_class_path = os.path.join(train_path, dataset_class)
+                for img in os.listdir(train_class_path):
+                    img_path = os.path.join(train_class_path, img)
+                    imgs.append(img_path)
+        _, val_dataset = train_test_split(imgs, test_size=int(dataset['val_size']) * 0.01)
+        for img_path in val_dataset:
+            path_splits = img_path.split('/')
+            path_splits[3] = path_splits[3].replace('train', 'val')
+            class_path = '/'.join(path_splits[:5])
+            if not os.path.isdir(class_path):
+                os.mkdir(class_path)
+            new_path = '/'.join(path_splits)
+            os.rename(img_path, new_path)
+            path_splits = img_path.split('/')
+            class_path = '/'.join(path_splits[:5])
+            if len(os.listdir(class_path)) == 0:
+                os.rmdir(class_path)
