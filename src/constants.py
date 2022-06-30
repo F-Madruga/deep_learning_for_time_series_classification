a16fa3a19e7488088e804303faf834dc51e565a1
FMadruga <madruga.francisco@protonmail.com>
2022-06-30 12:09:01 +0100

    add remaining dataset types

--- src/constants.py
+++ src/constants.py
@@ -2,6 +2,6 @@ RESULTS = 'results.csv'
 TRAIN_NEW_MODELS = False
 DATASETS_WITH_BAD_IMAGE_RESOLUTIONS = ['Computers', 'Earthquakes', 'ElectricDevices', 'FreezerRegularTrain', 'FreezerSmallTrain', 'Haptics', 'HouseTwenty', 'InlineSkate', 'InsectEPGRegularTrain', 'InsectEPGSmallTrain', 'LargeKitchenAppliances', 'Lightning2', 'Lightning7', 'Phoneme', 'RefrigerationDevices', 'ScreenType', 'SemgHandGenderCh2', 'SemgHandMovementCh2', 'SemgHandSubjectCh2', 'ShapeletSim']
 DATASETS_PATH = 'datasets'
-DATASET_TYPES = ['line_plots']
+DATASET_TYPES = ['line_plots', '5_boxplots', '5_boxplots_no_fliers', '8_boxplots', '10_boxplots', '12_boxplots', '20_boxplots']
 FAST_DEV_RUN = False
 GPUS = 2
